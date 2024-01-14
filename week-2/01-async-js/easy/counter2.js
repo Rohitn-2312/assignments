b41dc1780d3b8c1e27f0cb416b58055ba52a7ac4
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-01-15 01:00:20 +0530

    Update counter2.js

--- week-2/01-async-js/easy/counter2.js
+++ week-2/01-async-js/easy/counter2.js
@@ -2,3 +2,18 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+// Function to update the counter
+function updateCounter(count) {
+    console.log(count); 
+
+  function incrementCounter(count) {
+    count++; 
+
+    updateCounter(count); 
+    setTimeout(function() {
+      incrementCounter(count); 
+    }, 1000);
+  }
+  
+  incrementCounter(0);
+  
