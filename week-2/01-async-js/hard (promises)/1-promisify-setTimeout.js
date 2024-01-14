83af50c2f2499107770ddd06ad45c5c97cbeeb42
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-01-15 00:38:18 +0530

    Update 1-promisify-setTimeout.js

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,19 @@
 */
 
 function wait(n) {
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve(`Promise resolved after ${n} seconds`);
+        }, n*1000);
+    })
 }
+const secondsToWait=5;
+delay(secondsToWait)
+     .then(result=>{
+       console.log(result);
+     })
+     .catch(error=>{
+        console.error(error);
+     });
 
 module.exports = wait;
