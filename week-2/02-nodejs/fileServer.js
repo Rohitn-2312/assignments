a2429c4a2917f8a5b87ac7811910a2e25c7115f1
Rohit Nayak <110717323+Rohitn-2312@users.noreply.github.com>
2024-01-23 01:36:43 +0530

    Update fileServer.js

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,27 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get('/files',function(req,res){
+  fs.readdir(path.join(_dirname,'./files') (err,files)=>{
+    if(err){
+      return res.status(500).json(error:"Failed to retrieved file")
+    }
+    res.json(files);
+  });
+});
+
+app.get('/file/:filename' , function(req,res){
+  const filepath=path.json(_dirr,'./files/',req.params.filename);
+
+  fs.readFile(filepath,'utf8',(err,data) => {
+    if(err){
+      return res.status(404).send('File not found');
+    }
+    res.send(data);
+  });
+});
+app.all('*'),(req,res) => {
+  res.status(404).send('Route not found');
+});
 
 module.exports = app;
